1beda075d91c0939d305e779f4dcd45ed40d8d31
MC Vinding <mikkel.vinding@gmail.com>
2022-09-07 14:09:32 +0200

    re-coding eeg_field and plots

---
+++ data_summary.R
@@ -0,0 +1,133 @@
+# DATA SUMMARIES
+# Summaries of metadata variables and figures
+#
+# Ref: Position_paper
+#
+
+library(ggplot2)
+library(ggrepel)
+library(tidyverse)
+
+# Paths
+if ( Sys.getenv("USER") == 'mcvinding' ){
+  data.path <- '/Users/mcvinding/Documents/EEGManyPipelines/metadata_summary/data'
+}
+setwd(data.path)
+
+# Load data
+load('data.RData')
+
+################################################################################
+# Teams
+length(unique(data$team))
+tabTeam <- data.frame(tabulate(data$team))
+table(tabulate(data$team))
+mean(tabulate(data$team))
+
+ggplot(tabTeam, aes(x=tabulate.data.team.))+
+  geom_bar(fill="grey", colour="black")+
+  ggtitle('Team size')+
+  labs(x="Team size", y='')+
+  theme_bw()
+ggsave("teamSize.jpg", width = 3, height = 3, dpi=600)
+
+################################################################################
+# Age
+range(data$age)
+mean(data$age)
+median(data$age)
+sd(data$age)
+
+################################################################################
+# Gender
+table(data$gender)
+
+################################################################################
+# Number EEG papers
+range(data$eeg_papers)
+median(data$eeg_papers)
+mean(data$eeg_papers)
+sd(data$eeg_papers)
+
+ggplot(data, aes(x=eeg_papers))+
+  geom_histogram(binwidth=2, fill="grey", colour="black")+
+  ggtitle('Peer-reviewd EEG papers')+
+  scale_x_continuous(breaks=seq(0,150,25)) +
+  labs(x="N papers", y='')+
+  theme_bw()
+ggsave("EEGpapersAll.jpg", width = 6, height = 3, dpi=600)
+
+paperByTeam <- aggregate(data$eeg_papers, by=list(data$team), mean)
+range(paperByteam$x)
+median(paperByteam$x)
+mean(paperByTeam$x)
+sd(paperByTeam$x)
+
+ggplot(paperByTeam, aes(x=x))+
+  geom_histogram(binwidth=2, fill="grey", colour="black")+
+  ggtitle('Averge peer-reviewed EEG papers per team')+
+  labs(x="N papers", y='')+
+  scale_x_continuous(breaks=seq(0,60,10)) +
+  theme_bw()
+ggsave("EEGpapersTeam.jpg", width = 6, height = 3, dpi=600)
+  
+################################################################################
+# Years doing EEG
+range(data$eeg_years)
+median(data$eeg_years)
+mean(data$eeg_years)
+sd(data$eeg_years)
+
+teamExperience <- aggregate(data$eeg_years, by=list(data$team), sum)
+range(teamExperience$x)
+median(teamExperience$x)
+mean(teamExperience$x)
+sd(teamExperience$x)
+
+ggplot(teamExperience, aes(x=x))+
+  geom_histogram(binwidth=2, fill="grey", colour="black")+
+  ggtitle('Total number of years working with EEG per team')+
+  labs(x="Sum/Years", y='')+
+  scale_x_continuous(breaks=seq(0,60,10)) +
+  theme_bw()
+ggsave("EEGyearsTeam.jpg", width = 6, height = 3, dpi=600)
+
+################################################################################
+# Country
+length(unique(data$country))
+table(data$country)
+
+# The map figure...
+
+################################################################################
+# Academic field
+fieldData <- data.frame(table(data$eeg_field_edit))
+fieldData$pct  <- round(fieldData$Freq / sum(fieldData$Freq) * 100, 1)
+fieldData <- fieldData[order(-fieldData$Freq),]
+fieldData$Var1 <- reorder(fieldData$Var1, -fieldData$Freq)
+
+df2 <- fieldData %>% 
+  mutate(csum = rev(cumsum(rev(Freq))), 
+         pos = Freq/2 + lead(csum, 1),
+         pos = if_else(is.na(pos), Freq/2, pos))
+
+ggplot(df2, aes(x="", y=Freq, fill=Var1)) +
+  geom_bar(stat="identity", width=1, color="black") +
+  # geom_text(aes(label = paste0(pct, "%")),
+  #           position = position_stack(vjust = 0.5)) +
+  coord_polar("y", start=0) +
+  labs(x = NULL, y = NULL) +
+  geom_label_repel(aes(y = pos, label = paste0(Var1, " (",pct,"%)")),
+                   size = 4.5, nudge_x = 1, show.legend = FALSE) +
+  theme_void() +
+  theme(axis.ticks = element_blank(),
+        axis.title = element_blank(),
+        axis.text = element_blank(), 
+        legend.position = "none",
+        panel.background = element_rect(fill = "white"))
+
+ggsave("fieldPie.jpg", width = 6, height = 6, dpi=600)
+
+
+
+
--- import_data.R
+++ import_data.R
@@ -1,45 +1,100 @@
+### IMPORT PARTICIPANT METADATA
+# Read the raw csv file. Recode variables. Save in R format.
+#
+# Ref: Postion_paper
+#
 
-# Paths
-data.path <- '/Users/mcvinding/Documents/EEGManyPipelines/metadata_summary/sourcedata'
+# Paths and file
+if ( Sys.getenv("USER") == 'mcvinding' ){
+  proj.path <- '/Users/mcvinding/Documents/EEGManyPipelines/metadata_summary'
+}
+setwd(proj.path)
+
+data.path <- './sourcedata'
+out.path  <- './data'
 data.file <- 'demographic_cleaned.csv'
 
-# Read data
+# Read csv data
 data <- read.csv(file.path(data.path, data.file))
 
+###############################################################################
 # Order and re-code data
 data$team <- as.factor(data$team)
 
+# Country
 data$country <- ifelse(data$country=="United Arab Emirates", "UAE", data$country)
 data$country <- ifelse(data$country=="portugal", "Portugal", data$country)
 data$country <- ifelse(data$country=="The Netherlands", "Netherlands", data$country)
 data$country <- ifelse(data$country=="Korea", "Republic of Korea", data$country)
 data$country <- ifelse(data$country=="Catalonia", "Spain", data$country)
 
-# data$job_position $<- This one is tricky!
+# Re-code job position
+# data$job_position # <- This one is tricky!
 
-# SUMMARIES
-# Teams
-length(unique(data$team))
-table(tabulate(data$team))
-mean(tabulate(data$team))
+# Re-code academic field
+str_apneuro <- c("Applied Neuroscience", "clinical neuroscience", "Clinical Psychological Science ")
+str_psychia <- c("psychiatric neurophysiology", "Psychiatry")
+str_enginee <- c("Audio Engineering", "Engineering ", "DSP", "Neuroengineering", "signal processing")
+str_compsci <- c("Computer science", "computer science", "artificial intelligence")
+str_physics  <- c("physics")
+str_biomede <- c("biomedical engineer, biomedical signal processing engineer", "BIOMEDICAL ENGINEERING",
+  "Biomedical Engineering", "Biomedical engineering","biomedical engineering, neuroscience",
+  "Brain Computer Interfaces","neuroscience & machine learning", "Neuroimaging", "neuroimaging",
+  "Machine Learning for Neuroimaging Data Analysis")
+str_neursci <- c("Auditory Neuroscience and Hearing","behavioural genetics, neuroscience","neurosciences",
+  "human neuroscience", "NEUROSCIENCE", "Neuroscience", "neuroscience", "Neuroscience ","neuroscience EEG/fMRI",
+  "Neuroscience for learning disabilities", "neuroscience of (audiovisual) speech perception",
+  "neuroscience, biology", "neuroscience, cognitive psychology, digital signal processing, genetics",
+  "Neuroscience, Psychology", "Neuroscience, psychology", "neuroscience, psychology", 
+  "Neuroscience, psychology, neuropsychology", "Neuroscience, psychophysiology", 
+  "Neuroscience, Psychophysiology, Statistical Modelling","Neurosciences", "Neurosciences")
+str_neurolo <- c("neurology, neuroscience", "Neurophysiology", "neurophysiology","Electrophysiology")
+str_neruoim <- c()
+str_comneur <- c("Computational Neuroscience","Computational neuroscience","computational neuroscience",
+  "computational modelling")
+str_psychol <- c("Biological Psychology","biological Psychology","Cognitive psychology","cognitive psychology",
+  "episodic memory","Experimental Psychology","Experimental psychology","experimental psychology", "Experimental psychology ",
+  "experimental psychology, neurology","Neuropsychology","personality and individual differences",
+  "Personality Psychology, Psychophysiolgy and Neuroscience ","Psychology","psychology","Psychology ",
+  "psychology ","Psychology and Neuroscience","Psychology and neuroscience","psychology and neuroscience",
+  "Psychology, Cognitive Neuroscience","Psychology, cognitive neuroscience","psychology, developmental psychology, vision, cognition",
+  "Psychology, Individual Differences, Psychological Assessment, Personality","Psychology, neuroscience",
+  "psychology, neuroscience", "Psychology/educational neuroscience","Psychophysiology", "biological psychology")
+str_lingust <- c("cognitive neuroscience of language","Psycho- and neurolinguistics","Psycho/Neurolinguistics",
+  "cognitive neuroscience, linguistics","Cognitive Neuroscience/Psycholinguistics","Psycholinguistics",
+  "psycholinguistics","psychology of language")
+str_develop <- c("Developmental Psychology","Developmental Cognitive Neuroscience",
+  "Developmental cognitive neuroscience", "developmental cognitive neuroscience","developmental cognitive science",
+  "Human Development","Human development and family studies")
+str_biomedi <- c("Biomedical")
+str_elctphy <- c() 
+str_cognsci <- c("Cognitive Neuroscience","Cognitive neuroscience","cognitive neuroscience",
+  "Cognitive Neuroscience ","Cognitive neuroscience ","cognitive neuroscience ","Cognitive neuroscience / cognitive psychology",
+  "Cognitive Neuroscience / Psychology","cognitive neuroscience / psychology","cognitive neuroscience and neuropsychology",
+  "cognitive neuroscience and psychology","cognitive neuroscience, education, mathematical cognition",
+  "Cognitive Neuroscience, Psychology","cognitive neurosciences ","Cognitive Science","cognitive science",
+  "Computational Cognitive Science","social neuroscience","olfaction", "cognitive neuroscience, psychphysics", "decision making")
+str_marking <- c("marketing")
 
-# Age
-mean(data$age)
-median(data$age)
-range(data$age)
-sd(data$age)
+data$eeg_field_edit <- data$eeg_field
+data$eeg_field_edit <- ifelse(data$eeg_field_edit %in% str_apneuro, "Applied/clinical science",              data$eeg_field_edit)
+data$eeg_field_edit <- ifelse(data$eeg_field_edit %in% str_psychia, "Psychiatry",                            data$eeg_field_edit)
+data$eeg_field_edit <- ifelse(data$eeg_field_edit %in% str_enginee, "Engineering",                           data$eeg_field_edit)
+data$eeg_field_edit <- ifelse(data$eeg_field_edit %in% str_compsci, "Computer Science",                      data$eeg_field_edit)
+data$eeg_field_edit <- ifelse(data$eeg_field_edit %in% str_physics, "Physics",                               data$eeg_field_edit)
+data$eeg_field_edit <- ifelse(data$eeg_field_edit %in% str_biomede, "Biomedical Engineering",                data$eeg_field_edit)
+data$eeg_field_edit <- ifelse(data$eeg_field_edit %in% str_neursci, "Neuroscience",                          data$eeg_field_edit)
+data$eeg_field_edit <- ifelse(data$eeg_field_edit %in% str_neurolo, "Neurology/Neurophysiology",             data$eeg_field_edit)
+# data$eeg_field_edit <- ifelse(data$eeg_field_edit %in% str_neruoim, "Neuroimaging",                          data$eeg_field_edit)
+data$eeg_field_edit <- ifelse(data$eeg_field_edit %in% str_comneur, "Computational Neuroscience",            data$eeg_field_edit)
+data$eeg_field_edit <- ifelse(data$eeg_field_edit %in% str_psychol, "Psychology",                            data$eeg_field_edit)
+data$eeg_field_edit <- ifelse(data$eeg_field_edit %in% str_lingust, "Linguistics and language",              data$eeg_field_edit)
+data$eeg_field_edit <- ifelse(data$eeg_field_edit %in% str_develop, "Developmental Psychology/Neuroscience", data$eeg_field_edit)
+data$eeg_field_edit <- ifelse(data$eeg_field_edit %in% str_biomedi, "Biomedicine",                           data$eeg_field_edit)
+# data$eeg_field_edit <- ifelse(data$eeg_field_edit %in% str_elctphy, "Electrophysiology",                     data$eeg_field_edit)
+data$eeg_field_edit <- ifelse(data$eeg_field_edit %in% str_cognsci, "Cognitive Science",                     data$eeg_field_edit)
+data$eeg_field_edit <- ifelse(data$eeg_field_edit %in% str_marking, "Marketing",                             data$eeg_field_edit)
 
-# Country
-length(unique(data$country))
-table(data$country)
-
-# Gender
-table(data$gender)
-
-
-
-
-
-
-
-
+                 
+# Save data
+save(data, file=file.path(out.path, 'data.RData'))
