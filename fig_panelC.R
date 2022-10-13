9334a4e260dfea10d216e7ac445b91d1ab963e5c
Dr. Darinka Truebutschek <darinka.truebutschek@ad.aesthetics.mpg.de>
2022-10-13 17:54:16 +0200

    Gender distribution as a funciton of team size

--- fig_panelC.R
+++ fig_panelC.R
@@ -8,6 +8,9 @@
 #Necessary imports 
 library(tidyverse)
 library(optimbase)
+library(ggthemes)
+library(wesanderson)
+library(RColorBrewer)
 
 ################################################################################
 #Path definitions
@@ -77,9 +80,45 @@ dat2plot<-merge(dat2plot,df_teamsize,by="teamSize")
 dat2plot$proportions<-dat2plot$counts/dat2plot$vector_teamSizes
 
 #Plot
-theme_set(theme_bw())
-ggplot(dat2plot, aes(x=teamSize, fill=gender_recoded, y=counts)) +
-  geom_bar(stat='identity')
+#my_colors = c('#FFDF00', '#B40F20', '#046c9A', '#FFAA33')
+my_colors = c('#046c9A', '#046c9A', '#046c9A', '#046c9A')
+my_colors = brewer.pal(n=8, name='RdBu')[8:-1:5]
+my_alphas = c(1, 1, 1, 1)
+theme_set(theme_classic())
+
+g <- ggplot(dat2plot, aes(x=teamSize, fill=gender_recoded, y=counts)) +
+  geom_bar(stat='identity') +
+  #geom_text(aes(x=teamSize, y=counts, label=paste0(sprintf("%4.0f", round(proportions, digits=2)*100), '%'),
+                                                   #group=gender_recoded),
+            #position = position_stack(vjust=.5),
+            #family='sans', size=3, fontface='bold', color='white') +
+  geom_text(aes(x=teamSize, y=counts, label=sprintf("%4.0f", round(proportions, digits=2)*100),
+                                                    group=gender_recoded),
+            position = position_stack(vjust=0.5),
+            family='sans', size=6, fontface='bold', color='white') +
+  labs(x='Team size', y='Number of analysts', fill='Gender') +
+  theme(axis.title=element_text(size=18, color='dimgray'),
+        axis.text=element_text(size=16, color='dimgray')) +
+  ylim(c(0, 261)) +
+  theme(legend.title=element_text(size=16, color='dimgray', face='bold'), 
+        legend.position='top',
+        legend.text=element_text(size=16, color='dimgray')) +
+  #Re-order contents of legend to appear in the same order as the bars
+  guides(fill=guide_legend(reverse=TRUE, title.position='top', title.hjust=.5)) +
+  coord_flip() +
+  scale_x_discrete(limits=rev) +
+  scale_fill_manual(values=my_colors, labels=c('diverse', 'women', 'men', 'unknown')) +
+  #scale_alpha_manual(values=my_alphas) +
+  theme(aspect.ratio=.3)
+
+ggsave("Descriptives_PanelC.png", width = 6, height = 3, dpi=600)
+
+
+
+
+
+
+
 
 
 
