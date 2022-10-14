e8d963e09a078674ef73d3769da29d50b26174e3
Yu-Fang Yang <33165978+ufangYang@users.noreply.github.com>
2022-10-14 20:48:26 +0200

    upload the codes for the FIgC

--- data_summary.R
+++ data_summary.R
@@ -335,3 +335,101 @@ ggplot(df2, aes(x="", y=Freq, fill=Var1)) +
 ggsave("careerPie.jpg", width = 6, height = 6, dpi=600)
 
 ################################################################################
+
+################################################################################
+# regrouped-Academic levels vs team size 
+table(data$job_position_edit)
+
+jobData <- data.frame(table(data$job_position_edit))
+jobData$pct  <- round(jobData$Freq / sum(jobData$Freq) * 100, 1)
+jobData <- jobData[order(-jobData$Freq),]
+
+unique(data$job_position_edit)
+# 1. Pre-PhD student (i.e., only BSc & MSc students)
+# 2. PhD
+# 3. Post-doc
+# 4. Senior researchers and PI
+# 5. Others
+
+data  <-data %>% mutate( academic_levels =
+                          case_when(
+                            job_position_edit== "Pregraduate student"   ~ "Pre-PhD",
+                            job_position_edit== "PhD student"   ~ "PhD",
+                            job_position_edit== "Junior Professor/Researcher"  ~ "Senior researchers and PI",
+                            job_position_edit==  "Senior Professor"  ~ "Senior researchers and PI",
+                            job_position_edit==  "Lecturer/Instructor"  ~ "Senior researchers and PI",
+                            job_position_edit==  "Postdoc/Fellow/Scholar" ~ "Post-doc",
+                            
+                            job_position_edit==   "Research Assistant/Technician"   ~ "Industry/Research unite",
+                            job_position_edit==   "Outside academia"  ~ "Industry/Research unite",
+                            TRUE ~   job_position_edit
+                          ))
+
+unique(data$academic_levels) #here should be only 5 category
+
+# 1. add variable tracking team size 
+n_teams = length(unique(data$team))
+teams <- data$team
+team_sizes <- ones(length(teams), 1)
+
+for (value in seq(n_teams)) {
+  print(value)
+  
+  teamSize <- sum(teams == value)
+  team_sizes[teams == value] <- teamSize
+}
+
+data$teamSize <- team_sizes  
+data$teamSize <- as.factor(data$teamSize)
+
+# 2. extract academic level proportions
+data$academic_levels <- as.factor(data$academic_levels)
+
+
+table(data$academic_levels, by=data$teamSize)
+
+#Prepare data for plotting
+df_teamsize<-data.frame()
+df_teamsize[1:3,1]<-c(1,2,3)
+df_teamsize[1:3,2]<-c(31, 100, 261)
+colnames(df_teamsize)<-c("teamSize","vector_teamSizes")
+
+dat2plot <- data %>% 
+  select(academic_levels, teamSize) %>%
+  group_by(teamSize, academic_levels) %>%
+  summarise(counts=n())
+#mutate(proportions=counts/df_teamsize$vector_teamSizes)
+
+dat2plot<-merge(dat2plot,df_teamsize,by="teamSize")
+dat2plot$proportions<-dat2plot$counts/dat2plot$vector_teamSizes
+
+#Plot
+my_colors = c('#046c9A', '#046c9A', '#046c9A', '#046c9A')
+my_colors = brewer.pal(n=8, name='RdBu')[8:-1:5]
+my_alphas = c(1, 1, 1, 1)
+theme_set(theme_classic())
+
+g <- ggplot(dat2plot, aes(x=teamSize, fill=academic_levels, y=counts)) +
+  geom_bar(stat='identity') +
+  geom_text(aes(x=teamSize, y=counts, label=sprintf("%4.0f", round(proportions, digits=2)*100),
+                group=academic_levels),
+            position = position_stack(vjust=0.5),
+            family='sans', size=6, fontface='bold', color='white') +
+  labs(x='Team size', y='Number of analysts', fill='Academic level') +
+  
+  ylim(c(0, 261)) +
+  theme(legend.title=element_text(size=16, color='dimgray', face='bold'), 
+        legend.position='top',
+        legend.text=element_text(size=16, color='dimgray')) +
+  #Re-order contents of legend to appear in the same order as the bars
+  guides(fill=guide_legend(reverse=TRUE, title.position='top', title.hjust=.5)) +
+  coord_flip() +
+  scale_x_discrete(limits=rev) +
+  theme(aspect.ratio=.3) + 
+  theme(plot.margin=margin(t=0),
+        axis.title.y=element_text(angle=0, vjust=1.2,
+                                  margin=margin(t=0, r=-75, b=0, l=0)))+
+  theme(axis.title=element_text(size=18, color='dimgray'),
+        axis.text=element_text(size=16, color='dimgray')) 
+
+ggsave("Descriptives_PanelC.png", width = 6, height = 3, dpi=600)
