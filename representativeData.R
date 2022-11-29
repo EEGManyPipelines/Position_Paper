c3ce9bfb7bc6f93d04bdc69d40fe2c8c344fefa1
Dr. Darinka Truebutschek <darinka.truebutschek@ad.aesthetics.mpg.de>
2022-11-29 22:58:02 +0100

    Computing % of analysts per region

--- representativeData.R
+++ representativeData.R
@@ -37,13 +37,12 @@ setwd(data.path)
 
 ################################################################################
 #Load data
-data <- read.csv("percent_countries_pubmed_emp_figure.csv")
+data <- read.csv("percent_countries_pubmed_emp_figure_checkedDT.csv")
 
 ################################################################################
 #Plot data - vertical
 
-#my_colors = brewer.pal(n=8, name='RdBu')#[c(1,8)]
-my_colors = c('#B2182B', '#BABABA')
+my_colors = c('#69A19C', '#A9A9A9')
 my_alphas = c(1, 1, 1, 1)
 theme_set(theme_classic())
 
@@ -68,30 +67,43 @@ g <- ggplot(data, aes(fill=fill, x=continents, y=count, widths=.8))  +
   labs(x=' ', y='Percent', fill='Sample') +  
 
   #Change the aspect ratio of the plot 
-  theme(aspect.ratio=.7) + 
+  #theme(aspect.ratio=.7) + 
   
   #Remove unnecessary white space between axis and bars
   scale_x_discrete(expand=c(0,0)) + 
   scale_y_continuous(limits=c(0, 73), expand=c(0,0)) +
   
   #Change axis looks
-  theme(axis.title=element_text(size=16, color='dimgray', face='bold'),
-        axis.text=element_text(size=16, color='dimgray'), 
-        axis.line = element_line(color='dimgray', size=1, linetype='solid'),
-        axis.ticks = element_line(color='dimgray', size=1, linetype='solid')) +
+  #theme(axis.title=element_text(size=16, color='dimgray', face='bold'),
+        #axis.text=element_text(size=16, color='dimgray'), 
+        #axis.line = element_line(color='dimgray', size=1, linetype='solid'),
+        #axis.ticks = element_line(color='dimgray', size=1, linetype='solid')) +
         #axis.line.x = element_line(color='white', size=1, linetype='solid'),
         #axis.ticks.x = element_line(color='white', size=1, linetype='solid')) +
-  theme(plot.margin=unit(c(0.1,0,0,0), "null"),
-        axis.title.y=element_text(angle=0, vjust=1.075, margin=margin(t=0, r=-55, b=0, l=0))) 
+  #theme(plot.margin=unit(c(0.1,0,0,0), "null"),
+       #axis.title.y=element_text(angle=0, vjust=1.075, margin=margin(t=0, r=-55, b=0, l=0))) 
+
+#Change axis looks
+theme(axis.title=element_text(size=18, color='dimgray', face='bold'),
+      axis.text=element_text(size=18, color='dimgray'), 
+      axis.line = element_line(color='dimgray', size=1, linetype='solid'),
+      axis.ticks = element_line(color='dimgray', size=1, linetype='solid'),
+      plot.title = element_text(size=20, color='dimgray', face='bold', hjust=0.5),
+      legend.title=element_text(size=16, color='dimgray', face='bold'),
+      legend.text=element_text(size=14, color='dimgray'), 
+      #legend.position=c(.54, -.225),
+      axis.text.y=element_blank(),
+      aspect.ratio=1) 
 
   
 ggsave("Representative sample_vertical.png", width = 6, height = 4, dpi=600)
+ggsave("Representative sample_vertical.svg", width = 6, height = 4, dpi=600)
 
 
 
 ################################################################################
 #Plot data - horizontal
-my_colors = c('#BABABA', '#B2182B')
+my_colors = c('#BABABA', '#A9A9A9')
 
 g <- ggplot(data, aes(fill=fct_rev(fill), x=fct_rev(continents), y=count, widths=.8))  +
   geom_bar(stat='identity', position=position_dodge(.8)) +
