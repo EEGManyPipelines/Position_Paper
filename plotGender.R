c779c75a09aaabb665f024ab7c0a7c437036acbd
Yu-Fang Yang <33165978+ufangYang@users.noreply.github.com>
2022-12-06 14:38:03 +0100

    Update plotGender.R

--- plotGender.R
+++ plotGender.R
@@ -1,7 +1,7 @@
 #Purpose: Plot the gender distribution of the EEGManyPipes sample
 #Project: EEGManyPipes
 #Paper: Position Paper
-#Author: D. Truebutschek, M. Vinding, & Y. Yang
+#Author:  Y. Yang, D. Truebutschek, & M. Vinding, 
 #Date: 04-10-2022
 
 ################################################################################
@@ -167,101 +167,5 @@ pie <- g + coord_polar("y", start=0) +
 
 ggsave("EMP_Sample_Gender3.png", width=6, height=8, dpi=600)
 ggsave("EMP_Sample_Gender3.svg", width=6, height=8, dpi=600)
-
-
-################################################################################
-#Plot data - bars: Gender
-g <- ggplot(dat2plot, aes(x=teamSize, fill=gender_recoded, y=counts)) +
-  geom_bar(stat='identity') +
-  #geom_text(aes(x=teamSize, y=counts, label=paste0(sprintf("%4.0f", round(proportions, digits=2)*100), '%'),
-                                                   #group=gender_recoded),
-            #position = position_stack(vjust=.5),
-            #family='sans', size=3, fontface='bold', color='white') +
-  geom_text(aes(x=teamSize, y=counts, label=sprintf("%4.0f", round(proportions, digits=2)*100),
-                                                    group=gender_recoded),
-            position = position_stack(vjust=0.5),
-            family='sans', size=6, fontface='bold', color='white') +
-  labs(x='Team size', y='Number of analysts', fill='Gender') +
-  theme(axis.title=element_text(size=18, color='dimgray', face='bold'),
-        axis.text=element_text(size=16, color='dimgray')) +
-  #ylim(c(0, 261)) +
-  theme(legend.title=element_text(size=16, color='dimgray', face='bold'), 
-        legend.position='top',
-        legend.justification='right',
-        legend.margin=margin(t=0),
-        legend.text=element_text(size=16, color='dimgray')) +
-  #Re-order contents of legend to appear in the same order as the bars
-  guides(fill=guide_legend(reverse=TRUE, title.position='top', title.hjust=.5)) +
-  coord_flip() +
-  scale_x_discrete(limits=rev, expand=c(0,0)) + 
-  scale_y_continuous(limits=c(-1, 262), expand=c(0,0)) +                   
-  scale_fill_manual(values=my_colors, labels=c('diverse', 'women', 'men', 'unknown')) +
-  #scale_alpha_manual(values=my_alphas) +
-  theme(aspect.ratio=.3) + 
-  theme(plot.margin=margin(t=0),
-        axis.title.y=element_text(angle=0, vjust=1.2,
-                                  margin=margin(t=0, r=-75, b=0, l=0)))
-
-ggsave("Gender.png", width = 6, height = 4, dpi=600)
-
-################################################################################
-#Plot data - percentages: Gender
-
-#Job position
-g <- ggplot(dat2plot, aes(x=teamSize, y=gender_recoded)) +
-  geom_raster(aes(fill=proportions)) +
-  
-  #Change order of labels in axis
-  scale_y_discrete(limits = c('Unknown', 'diverse', 'female', 'male'), 
-                   labels = c('Unknown', 'Diverse', 'Women', 'Men')) +
-  
-  #Change color
-  scale_fill_gradient(limits=c(0, 0.7), breaks=c(0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7), labels=c('0', '10', '20', '30', '40', '50', '60', '70'),
-                      low='#C3CDDC', high='#38598A') +
-  
-  #Add percentage labels to tiles
-  geom_text(aes(x=teamSize, y=gender_recoded, label=paste0(sprintf("%4.0f", round(proportions*100, digits=0))),
-                group=value), vjust=0.5, hjust=0.65,
-            family='sans', size=8, fontface='bold', color='white') +
-  
-  #Change theme
-  theme_classic() +
-
-  labs(x='Team size', y=' ') + 
-  
-  #Change legend
-  guides(fill=guide_colorbar(title='Relative percentage', title.position='top',
-                             title.vjust=0.1, title.hjust=0.5, direction='horizontal', nbin=1000)) +
-  
-  #Add title
-  ggtitle('Analysts gender') +
-  
-  #Change axis looks
-  theme(axis.title=element_text(size=20, color='dimgray', face='bold'),
-        axis.text=element_text(size=20, color='dimgray'), 
-        axis.line = element_line(color='dimgray', size=1, linetype='solid'),
-        axis.ticks = element_line(color='dimgray', size=1, linetype='solid'),
-        plot.title = element_text(size=22, color='dimgray', face='bold', hjust=0.5),
-        legend.title=element_text(size=18, color='dimgray', face='bold'),
-        legend.text=element_text(size=18, color='dimgray'), 
-        legend.position=c(.54, -.225),
-        axis.text.y=element_blank(),
-        aspect.ratio=1) 
-
-ggsave("EMP_Sample_Gender.png", width=6, height=8, dpi=600)
-ggsave("EMP_Sample_Gender.svg", width=6, height=8, dpi=600)
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
